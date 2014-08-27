b1b778c095686f405bb743ff61f1d7738590a1af
liulixiang1988 <liulixiang1988@gmail.com>
2014-08-27 09:54:16 +0800

    L06 Intent

---
+++ L06Intent/src/liulx/L06Intent/Aty1.java
@@ -0,0 +1,15 @@
+package liulx.L06Intent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2014/8/27.
+ */
+public class Aty1 extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty1);
+    }
+}
